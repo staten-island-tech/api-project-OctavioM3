aad1ae89e05798c8fa72df67a9fd7eb4960f709d
OctavioM3 <octaviom3@nycstudents.net>
2023-12-06 09:22:11 -0500

    fgjhjfjjhg

--- api-project/main.js
+++ api-project/main.js
@@ -1,13 +1,14 @@
-const URL = `https://stats.nba.com/playercareerstats`
+const URL = `https://stats.nba.com/stats/playercareerstats`
 
 async function getData(URL){
   try {
     const response = await fetch(URL);
     console.log(response);
     const data = await response.json();
-    console.log(data)
+    console.log(data);
+    document.querySelector("h1").textContent = data.content;
   } catch (error) {
-
+    
   }
 }
-getData(URL)
+getData(URL);
