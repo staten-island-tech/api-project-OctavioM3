fe9790ca09557f55baaf4dae2c8b4b068c19513c
OctavioM3 <octaviom3@nycstudents.net>
2023-12-07 09:24:33 -0500

    hgfjgfjhjjhfghjgj

---
+++ api-project/js/main.js
@@ -0,0 +1,17 @@
+const URL = `https://botw-compendium.herokuapp.com/api/v3/compendium/all`
+
+async function getData(URL){
+  try {
+    const response = await fetch(URL);
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    document.querySelector("h1").textContent = error
+  }
+}
+
+getData(URL);
