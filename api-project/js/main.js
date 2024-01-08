f8721f0e0b18249400f704f9c16d6747e24992bd
OctavioM3 <octaviom3@nycstudents.net>
2024-01-08 09:24:19 -0500

    gsgdgdfg

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,41 +1,46 @@
 import "../css/style.css";
 import { DOMSelectors } from "./dom";
-const EquipmentURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment`
-const MonsterURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters`
-const MaterialURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials`
+const EquipmentURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment`;
+const MonsterURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters`;
+const MaterialURL = `https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials`;
 const response = await fetch(EquipmentURL);
-const secresponse = await fetch(MonsterURL);
-const thirdresponse = await fetch(MaterialURL);
-const data = await response.json();
-const mondata = await secresponse.json();
-const materialdata = await thirdresponse.json();
+const secresponse = fetch(MonsterURL);
+const thirdresponse = fetch(MaterialURL);
+const data = response.json();
+const mondata = secresponse.json();
+const materialdata = thirdresponse.json();
 
-async function getData(EquipmentURL, MonsterURL, MaterialURL){
+async function getData(EquipmentURL, MonsterURL, MaterialURL) {
   try {
     const response = await fetch(EquipmentURL);
     const secresponse = await fetch(MonsterURL);
     const thirdresponse = await fetch(MaterialURL);
-    if (response.status != 200, secresponse.status != 200, thirdresponse.status != 200) {
-      throw new error(response.statusText, secresponse.statusText, thirdresponse.statusText);
+    if (
+      (response.status != 200,
+      secresponse.status != 200,
+      thirdresponse.status != 200)
+    ) {
+      throw new error(
+        response.statusText,
+        secresponse.statusText,
+        thirdresponse.statusText
+      );
     }
     console.log(response);
     const data = await response.json();
     console.log(data);
     console.log(secresponse);
     const mondata = await secresponse.json();
-    console.log(mondata)
+    console.log(mondata);
     console.log(thirdresponse);
     const materialdata = await thirdresponse.json();
     console.log(materialdata);
-    } catch (error) {
-    document.querySelector("h2").textContent = error
+  } catch (error) {
+    document.querySelector("h2").textContent = error;
   }
 }
 
-
 getData(EquipmentURL, MonsterURL, MaterialURL);
-
-
 
 DOMSelectors.EquipmentBtn.addEventListener("click", function (event) {
   event.preventDefault();
@@ -49,7 +54,7 @@ DOMSelectors.MonsterBtn.addEventListener("click", function (event) {
   inputMonsters();
 });
 
-DOMSelectors.MaterialBtn.addEventListener("click", function(event) {
+DOMSelectors.MaterialBtn.addEventListener("click", function (event) {
   event.preventDefault();
   clearall();
   inputMaterials();
@@ -61,23 +66,31 @@ function clearall() {
 }
 
 function inputName() {
-  data.data.forEach((data) => DOMSelectors.ItemBox.insertAdjacentHTML(
-  "beforeend",
-  `<div class="ItemCard"><h2 id="Name"> ${data.name} </h2><img src="${data.image}" alt="${data.name}" class="Cardimg"></img><p class="description">${data.description}</p></div>`));
-};
+  data.data.forEach((data) =>
+    DOMSelectors.ItemBox.insertAdjacentHTML(
+      "beforeend",
+      `<div class="ItemCard"><h2 id="Name"> ${data.name} </h2><img src="${data.image}" alt="${data.name}" class="Cardimg"></img><p class="description">${data.description}</p></div>`
+    )
+  );
+}
 
 function inputMonsters() {
-  mondata.data.forEach((mondata) => DOMSelectors.ItemBox.insertAdjacentHTML(
-    "beforeend",
-    `<div class="ItemCard"><h2 id="Name"> ${mondata.name} </h2><img src="${mondata.image}" alt="${mondata.name}" class="Cardimg"></img><p class="description">${mondata.description}</P> <p class="drops">Drops: ${mondata.drops}</p></div>`
-  ));
-};
+  mondata.data.forEach((mondata) =>
+    DOMSelectors.ItemBox.insertAdjacentHTML(
+      "beforeend",
+      `<div class="ItemCard"><h2 id="Name"> ${mondata.name} </h2><img src="${mondata.image}" alt="${mondata.name}" class="Cardimg"></img><p class="description">${mondata.description}</P> <p class="drops">Drops: ${mondata.drops}</p></div>`
+    )
+  );
+}
 
 function inputMaterials() {
-  materialdata.data.forEach((materialdata) => DOMSelectors.ItemBox.insertAdjacentHTML(
-    "beforeend",
-    `<div class="ItemCard"><h2 id="Name"> ${materialdata.name} </h2><img src="${materialdata.image}" alt="${materialdata.name}" class="Cardimg"></img><p class="description">${materialdata.description}</p> <p class="location">${materialdata.common_locations}</p></div>`));
-  };
+  materialdata.data.forEach((materialdata) =>
+    DOMSelectors.ItemBox.insertAdjacentHTML(
+      "beforeend",
+      `<div class="ItemCard"><h2 id="Name"> ${materialdata.name} </h2><img src="${materialdata.image}" alt="${materialdata.name}" class="Cardimg"></img><p class="description">${materialdata.description}</p> <p class="location">${materialdata.common_locations}</p></div>`
+    )
+  );
+}
 /*
   function ButtonChange (data, mondata, materialdata) {
 let buttons = document.querySelectorAll(".btn")
